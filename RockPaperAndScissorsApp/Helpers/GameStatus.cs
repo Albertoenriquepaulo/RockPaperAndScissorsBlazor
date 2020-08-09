0e60e3a7cc83436e21e0ae6009db30700dbcf412
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-08-09 22:42:57 +0200

    19. Timers and IDisposable

---
+++ RockPaperAndScissorsApp/Helpers/GameStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperAndScissorsApp.Helpers
+{
+    public static class GameStatus
+    {
+        public static readonly string Victory = "You Won";
+        public static readonly string Loss = "You Lost";
+        public static readonly string Draw = "Draw";
+        //Victory, Loss, Draw
+    }
+}
---
+++ RockPaperAndScissorsApp/Helpers/Hand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperAndScissorsApp.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinsAgainst { get; set; }
+        public OptionRPS LosesAgainst { get; set; }
+        public string Image { get; set; }
+        public string Border { get; set; }
+
+        public string PlayAgainst(Hand opponnennHand)
+        {
+            if (Selection == opponnennHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            if (LosesAgainst == opponnennHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+            return GameStatus.Victory;
+        }
+    }
+}
